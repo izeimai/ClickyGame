f7d2dcee20f43ee6b9b3e84b55d48b4a781a9be4
Ize Imai <izeimai@gmail.com>
2019-03-29 20:09:07 -0400

    Added png in a img folder and removed logo from the generic app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import images from "./images.json";
 import './App.css';
 
 class App extends Component {
+
+  state = {images};
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <h1 className="title">Alphabet Image List</h1>
+        {this.state.images.map( images => (<ImageCard images={images}
+        removeImage = {this.removeImage}/>))}
+        />
+      </Wrapper >
     );
   }
 }
