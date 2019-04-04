4e21ea7a18500b43a5bd7a218923d218c9980488
Ize Imai <izeimai@gmail.com>
2019-04-03 22:32:41 -0400

    Updates

---
+++ precache-manifest.0c4d58c4ab5c667470c93626b1336cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "c9a00da6fb2011d8bdc6",
+    "url": "/clickygame/static/js/main.ec360a3b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c9a00da6fb2011d8bdc6",
+    "url": "/clickygame/static/css/main.508f643c.chunk.css"
+  },
+  {
+    "revision": "3fb4db3d6fb1dbff9e58b8b113f01de5",
+    "url": "/clickygame/index.html"
+  }
+];
